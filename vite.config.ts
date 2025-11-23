cb13df693bf81c510a0c0cf7656f405f7ce0cbbb
ianicius <36769332+ianicius@users.noreply.github.com>
2025-11-23 15:31:27 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/aidiofy-site/,
+  base: '/aidiofy-site/',   // exact repo name
 })
